8c73e84b91322f4a475a5bf69fca581269c27343
Gabriel Gilder <gabriel@gabrielgilder.com>
2024-09-09 11:17:41 -0700

    Interactive mode ✨

--- things-triage.rb
+++ things-triage.rb
@@ -12,15 +12,53 @@ require "action_view"
 
 include ActionView::Helpers::DateHelper
 
-today = Appscript.app("Things3").lists['Today'].to_dos.get.map do |to_do|
-  [
-    to_do.id_.get,
-    to_do.name.get,
-    to_do.creation_date.get,
-  ]
+class TodoList
+  def initialize(list)
+    @list = list
+    @todos = []
+    @app = Appscript.app("Things3")
+    refresh
+  end
+
+  def refresh
+    @todos = @app.lists[@list].to_dos.get.sort_by { |to_do| to_do.creation_date.get }
+  end
+
+  def display
+    @todos.each_with_index do |to_do, index|
+      puts "#{index + 1}. " + time_ago_in_words(to_do.creation_date.get) + ": #{to_do.name.get}"
+    end
+    puts "Total: #{@todos.count}"
+  end
+
+  def show(index)
+    @todos[index].show
+    @app.activate
+  end
+
+  def count
+    @todos.count
+  end
 end
 
-today.sort_by { |to_do| to_do[2] }.each do |to_do|
-  puts time_ago_in_words(to_do[2]) + ": #{to_do[1]}"
+
+today = TodoList.new('Today')
+today.display
+
+while true do
+  puts "Select a task to show, [r]efresh, or [q]uit"
+  print "> "
+  selected = gets
+  case selected.to_s.chomp
+  when /\d+/
+    selected = selected.to_i
+    if selected > 0 && selected <= today.count
+      today.show(selected - 1)
+    end
+  when /[rR]/
+    today.refresh
+    today.display
+  when /[qQ]/
+    break
+  end
 end
-puts "Total: #{today.count}"
